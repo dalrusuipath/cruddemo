ed16594be37180c326680767c3c6dcd94f3afa23
Dalton <dalton.russell@uipath.com>
2023-03-13 16:45:22 -0500

    Removed respository

--- cruddemo/src/main/java/com/luv2code/springboot/cruddemo/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luv2code.springboot.cruddemo.repository;
-
-import com.luv2code.springboot.cruddemo.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-    // thats it ... no need to write anything else as we get crud from spring data jpa
-}
